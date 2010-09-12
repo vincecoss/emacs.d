5bf455283ed02a71ba4302c9fe89e07c4eddf51c
vincecoss <vince.coss@gmail.com>
2010-09-12 18:25:17 -0400

    Add Yasnippet

--- custom.el
+++ custom.el
@@ -1,8 +1,14 @@
 ;;; OS X specific config
 (setq mac-command-modifier 'meta)
 
-;;; PHP
+;;; Plugins
+(add-to-list 'load-path "~/.emacs.d/plugins/")
 (require 'php-mode)
+
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
 
 ;;; pref
 (color-theme-twilight)
