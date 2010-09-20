63033397800a50297d2765b4d3a7f0be34d772e5
vincecoss <vince.coss@gmail.com>
2010-09-19 20:59:35 -0400

    Update backspace/delete keys actions and display column/line number

--- custom.el
+++ custom.el
@@ -1,12 +1,15 @@
 ;; bring back menu bar
 (menu-bar-mode 1) 
 
-;;; OS X specific config
+;; OS X specific config
 (setq mac-command-modifier 'meta)
 
-;;; Plugins
-(add-to-list 'load-path "~/.emacs.d/plugins/")
-(require 'php-mode)
+;; Custom keybindings
+(normal-erase-is-backspace-mode 1) ; proper backspace/delete
+
+;; Plugins
+      (add-to-list 'load-path "~/.emacs.d/plugins/")
+      (require 'php-mode)
 
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
 (require 'yasnippet)
@@ -15,3 +18,6 @@
 
 ;;; pref
 (color-theme-twilight)
+
+(column-number-mode t)
+(line-number-mode t)
