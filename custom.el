d2348c73ca45b70f06fd48b738eabfffe258a817
vincecoss <vince.coss@gmail.com>
2010-09-20 21:49:59 -0400

    Rename plugins to vendor

--- custom.el
+++ custom.el
@@ -8,13 +8,13 @@
 (normal-erase-is-backspace-mode 1) ; proper backspace/delete
 
 ;; Plugins
-      (add-to-list 'load-path "~/.emacs.d/plugins/")
-      (require 'php-mode)
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+(require 'php-mode)
 
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet-0.6.1c")
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
 
 ;; Tramp to edit files over ssh
 (require 'tramp)
