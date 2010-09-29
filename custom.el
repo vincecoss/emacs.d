76e332edd02dce5ef097c7e2bcf3c233bf3d09a4
vincecoss <vince.coss@gmail.com>
2010-09-29 19:24:06 -0400

    Add textmate mode and slime/sbcl

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
 
 ;; OS X specific config
 (setq mac-command-modifier 'meta)
+;;(setq mac-option-modifier 'meta)
 
 ;; Custom keybindings
 (normal-erase-is-backspace-mode 1) ; proper backspace/delete
@@ -15,6 +16,20 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
+
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+(require 'textmate)
+(textmate-mode)
+
+;; Lisp
+(setq inferior-lisp-program "/usr/local/bin/sbcl") ; your Lisp system
+(add-to-list 'load-path "~/Library/Slime/slime/")  ; SLIME directory
+(require 'slime)
+(setq slime-net-coding-system 'utf-8-unix
+      lisp-indent-function 'common-lisp-indent-function
+      slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+(slime-setup '(slime-fancy))
+
 
 ;; Tramp to edit files over ssh
 (require 'tramp)
